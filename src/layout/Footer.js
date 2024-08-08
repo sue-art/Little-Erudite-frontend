acd2d61eecb573a5230d98befceef928dabd0edd
sue-art <suepark0305@gmail.com>
2024-08-08 15:39:16 +1000

    Apply Link to footer link

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = ({ toggleTheme, state }) => {
   const logo = window.location.origin + "/logo_bottom.png";
@@ -7,39 +8,40 @@ const Footer = ({ toggleTheme, state }) => {
     <footer className="dairy-cream dark">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <img src={logo} alt="Little Erudite" width="130" />
-          </a>
+          </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="/about" className="hover:underline me-4 md:me-6">
-                About
-              </a>
+              <Link to="/about" className="hover:underline me-4 md:me-6">
+                About us
+              </Link>
             </li>
             <li>
-              <a
-                href="/privacy-policy"
+              <Link
+                to="/privacy-policy"
                 className="hover:underline me-4 md:me-6"
               >
                 Privacy Policy
-              </a>
+              </Link>
             </li>
+
             <li>
-              <a href="/contact" className="hover:underline">
-                Contact
-              </a>
+              <Link to="/contact" className="hover:underline me-4 md:me-6">
+                Contact us
+              </Link>
             </li>
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2024{" "}
-          <a href="/" className="hover:underline">
+          <Link to="/" className="hover:underline">
             Little Erudite
-          </a>
+          </Link>
           . All Rights Reserved.
         </span>
       </div>
